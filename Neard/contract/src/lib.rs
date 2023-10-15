193b2b88c4210c33124fcdb36fd520150af2f9f2
TechieTeee <techieteee@protonmail.com>
2023-10-15 05:53:24 +0000

    Updated smart contract

--- Neard/contract/src/lib.rs
+++ Neard/contract/src/lib.rs
@@ -1,64 +1,66 @@
-// Find all our documentation at https://docs.near.org
+// SPDX-License-Identifier: MIT
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::collections::{LookupMap, UnorderedMap};
+use near_sdk::json_types::U128;
+use near_sdk::{env, near_bindgen, AccountId, Balance};
 
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
+extern crate flate2;
 
-// Define the contract structure
-#[near_bindgen]
+use flate2::{write::ZlibEncoder, Compression};
+
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    message: String,
+struct DataRecord {
+    id: u64,
+    compressed_data: Vec<u8>,
 }
 
-// Define the default, which automatically initializes the contract
-impl Default for Contract {
-    fn default() -> Self {
-        Self { message: DEFAULT_MESSAGE.to_string() }
-    }
-}
-
-// Implement the contract structure
 #[near_bindgen]
-impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
-    }
+pub struct DataEngine {
+    records: UnorderedMap<u64, DataRecord>,
+    balances: LookupMap<AccountId, Balance>,
+}
 
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        log!("Saving greeting {}", message);
-        self.message = message;
+impl Default for DataEngine {
+    fn default() -> Self {
+        Self {
+            records: UnorderedMap::new(b"data_records"),
+            balances: LookupMap::new(b"account_balances"),
+        }
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn deflate_data(data: String) -> Result<Vec<u8>, String> {
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(data.as_bytes()).map_err(|e| e.to_string())?;
+    encoder.finish().map_err(|e| e.to_string())
+}
 
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
+fn inflate_data(compressed_data: Vec<u8>) -> Result<String, String> {
+    let mut decoder = flate2::read::ZlibDecoder::new(&compressed_data[..]);
+    let mut decompressed_data = String::new();
+    decoder.read_to_string(&mut decompressed_data).map_err(|e| e.to_string())?;
+    Ok(decompressed_data)
+}
+
+#[near_bindgen]
+impl DataEngine {
+    pub fn store_record(&mut self, data: String) -> Result<u64, String> {
+        let id = self.records.len() + 1;
+        let compressed_data = deflate_data(data)?;
+        let record = DataRecord { id, compressed_data };
+        self.records.insert(&id, &record);
+        Ok(id)
     }
 
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
+    pub fn get_record(&self, id: u64) -> Option<Result<String, String>> {
+        self.records.get(&id).map(|record| inflate_data(record.compressed_data))
+    }
+
+    pub fn charge_for_data(&mut self, account_id: AccountId, amount: U128) {
+        if let Some(mut balance) = self.balances.get(&account_id) {
+            balance -= amount.0;
+            self.balances.insert(&account_id, &balance);
+        }
     }
 }
